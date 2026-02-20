cdccc3cc5ce9636beb9e1ae86821cb0aec7cb5b4
Rahul <kanchanareddyrahulagmai.com@gmail.com>
2026-02-20 15:30:50 +0530

    Added a new feature

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,13 @@
-// Hint: Use the Example below as a guide to construct each line of the banner
-// Example for OS which stands for Operating System:
+public class OOPSBannerApp {
+    public static void main(String[] args) {
 
-System.out.println("  ***   " + " " + " ***** ");
-System.out.println(" ** **  " + " " + " **    ");
-System.out.println("**   ** " + " " + " **    ");
-System.out.println("**   ** " + " " + " ***** ");
-System.out.println("**   ** " + " " + "    ** ");
-System.out.println(" ** **  " + " " + " ** ** ");
-System.out.println("  ***   " + " " + " ***** ");
+        System.out.println("   ***       ***      ******      *******  ");
+        System.out.println("  **   **   **   **    **   **    **      ");
+        System.out.println(" **     ** **     **   **   **    **      ");
+        System.out.println(" **     ** **     **   ******      *****  ");
+        System.out.println(" **     ** **     **   **               ** ");
+        System.out.println("  **   **   **   **    **         **    ** ");
+        System.out.println("   ***       ***      **           *****  ");
+
+    }
+}
